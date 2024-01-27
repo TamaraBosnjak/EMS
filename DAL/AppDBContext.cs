14d9a8018d0edb739987b0971c981d850ad418be
Tamara Bosnjak <bosnjakt@yahoo.com>
2024-01-27 16:02:51 +0100

    Added bootstrap and jquery

---
+++ DAL/AppDBContext.cs
@@ -0,0 +1,17 @@
+﻿using EmployeeManagementSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagementSystem.DAL
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : 
+            base(options)
+        {
+        }
+        
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<JobRole> JobRoles { get; set; }
+        public DbSet<Department> Departments { get; set; }
+    }
+}
